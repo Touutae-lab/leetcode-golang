c33b1ff89ecbb478486fe2a6d311cfcd3e566705
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 23:58:25 +0700

    add current code

---
+++ 238 Product of Array Except Self/238.go
@@ -0,0 +1,26 @@
+package main
+
+func productExceptSelf(nums []int) []int {
+	var prefix = make([]int, len(nums))
+	var suffix = make([]int, len(nums))
+
+	prefix[0] = nums[0]
+	suffix[len(nums)-1] = nums[len(nums)-1]
+	for i := 1; i < len(nums); i++ {
+		prefix[i] = prefix[i-1] * nums[i]
+		suffix[len(nums)-1-i] = suffix[len(nums)-i] * nums[len(nums)-1-i]
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if i == 0 {
+			nums[i] = suffix[i+1]
+		} else if i == len(nums)-1 {
+			nums[i] = prefix[i-1]
+		} else {
+			nums[i] = prefix[i-1] * suffix[i+1]
+		}
+	}
+
+	return nums
+
+}
